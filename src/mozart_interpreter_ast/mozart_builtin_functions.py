cd08db127cbabc4b27ddd8ec9696237b20272654
Tobias Rossi Muller <trossimu@emeal.nttdata.com>
2024-01-12 11:49:11 -0300

    improve the description of builtin functions

--- src/mozart_interpreter_ast/mozart_builtin_functions.py
+++ src/mozart_interpreter_ast/mozart_builtin_functions.py
@@ -3,14 +3,25 @@ from mozart_ast import ValueType
 
 
 def belong(params, program_state):
+    """
+    Check if a note belongs to a chord or
+          if a note belongs to a scale or
+          if a chord belongs to a harmonic field.
+    """
     pass
 
 
 def transpose(params, program_state):
+    """
+    transpose a note, chord, scale, harmonic_field, music in x semitones
+    """
     pass
 
 
 def get(params, program_state):
+    """
+    A function to get a specific value from a not primitive mozart type
+    """
     accepted_types = [ValueType.HARMONIC_FIELD, ValueType.CHORD, ValueType.NOTE, ValueType.SCALE]
     if len(params) != 2:
         raise WrongNumberOfParams(2, len(params))
@@ -23,9 +34,16 @@ def get(params, program_state):
 
     return params[0][1][params[1][1]]
 
+
 def append_note(params, program_state):
+    """
+    Append a note into a mozart scale or music object
+    """
     pass
 
 
 def append_chord(params, program_state):
+    """
+    Append a chord into a harmonic field
+    """
     pass
